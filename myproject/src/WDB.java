b8419c8db9f6f521d50b7827d93c704c5c23b30d
WDB <hhj5828@naver.com>
2016-11-04 00:36:47 +0900

    qw
    qw

--- myproject/src/WDB.java
+++ myproject/src/WDB.java
@@ -2,51 +2,63 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 
-
 class myframe extends JFrame implements ActionListener{
-	JPanel p1,p2;
-	JLabel lstart;
-	JButton bstart;
-	JTextField tfstart;
+	JPanel p1,p2,p3;
+	JLabel l1,l2;
+	JTextField tf1,tf2;
+	JButton b1;
+	int v;
+	String name;
 	public myframe(){
-		setTitle("시작 화면");
-		setSize(300,200);
+		v = 0;
+		setTitle("그림맞추기게임");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setLayout(new BorderLayout());
+		setSize(180,200);
+		
 		p1 = new JPanel();
+		
+		l1 = new JLabel("이름을 입력하세요");
+		
+		tf1 = new JTextField(10);
+		
+		l2 = new JLabel("카드 숫자를 입력하자");
+		
+		tf2 = new JTextField(5);
+		
+		b1 = new JButton("게임 시작");
+	
+		b1.addActionListener(this);
+		p1.add(l1);
+		p1.add(tf1);
+		p1.add(l2);
+		p1.add(tf2);
+		p1.add(b1);
+		add(p1);
+		
 		p2 = new JPanel();
-		lstart = new JLabel("이름 적고 시작하세요");
-		tfstart = new JTextField(20);
-		p1.add(lstart);
-		p1.add(tfstart);
-		bstart = new JButton("게임 시작");
-		p2.add(bstart);
-		bstart.addActionListener(this);
-		add(p1,BorderLayout.CENTER);
-		add(p2,BorderLayout.SOUTH);
+		p2.setLayout(new GridLayout(0,3));
+		
+		p2.setVisible(false);
+		setResizable(false);
 		setVisible(true);
 	}
 	@Override
-	public void actionPerformed(ActionEvent e) {
+	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-		JFrame yourframe = new JFrame("게임화면");
-		yourframe.setSize(1000, 500);
-		yourframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		yourframe.setLayout(new GridLayout(0,3));
-		Object[] array = new Object[15];
-		for(int i = 0;i<15;i++){
-			array[i] = yourframe.add(new JButton("a"+i));
+		System.out.println("버튼이 클릭됨");
+		v = Integer.parseInt(tf2.getText());
+		p1.setVisible(false);
+		name = tf1.getText();
+		
+		this.setSize(1000,500);
+		System.out.println(name);
+		System.out.println(v+5);
+		for(int i = 0;i<v;i++){
+			p2.add(new JButton("버튼"+i));
 		}
-		ImageIcon[] nope1 = new ImageIcon[7];
-		for(int i = 0;i<nope1.length;i++){
-			nope1[i] = new ImageIcon((i+1)+".jpg");
-		}
-		Object[] array1 = new Object[nope1.length];
-		for(int i = 0;i<array1.length;i++){
-			array1[i] = nope1[(int) (Math.random()*array1.length)];
-		}
-		yourframe.setVisible(true);
-		this.setVisible(false);
+		p2.setVisible(true);
+		setResizable(true);
+		add(p2);
 	}
 }
 
